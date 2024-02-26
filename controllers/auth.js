b4f307d37fbbfe4489cf0b950a4c0e2722e22273
NattawitMana <6633066721@student.chula.ac.th>
2024-02-26 22:00:24 +0700

    slide 11

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,4 +5,22 @@ const User = require('../models/User');
 //@access   Public
 exports.register=(req,res,next)=> {
     res.status(200).json({sucess:true});
+};
+exports.register=async (req,res,next)=>{
+    try{
+        const {name,email,password,tel,role}=req.body;
+
+        //Create user
+        const user=await User.create({
+            name,
+            email,
+            password,
+            tel,
+            role
+        });
+        res.status(200).json({success:true});
+    } catch(err) {
+        res.status(400).json({success:false});
+        console.log(err.stack);
+    }
 }
