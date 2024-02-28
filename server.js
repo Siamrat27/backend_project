140337b8f5a0163a6f9855d7ce279c91ea682e69
NattawitMana <6633066721@student.chula.ac.th>
2024-02-28 15:22:21 +0700

    finish chapter12

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser=require('cookie-parser');
 const connectDB = require('./config/db');
+const mongoSanitize=require('express-mongo-sanitize');
+const helmet=require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp');
+const cors=require('cors');
+
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -22,6 +29,28 @@ app.use(express.json());
 
 //Cookie parser
 app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter=rateLimit({
+    windowsMs:10*60*1000,//10 mins
+    max: 500
+    });
+app.use(limiter);  
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 app.use('/api/v1/coworkingspaces',coworkingspaces);
 app.use('/api/v1/auth',auth);
